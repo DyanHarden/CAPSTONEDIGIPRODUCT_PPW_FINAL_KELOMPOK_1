b62d62d5cc946a9e87db158b8204b6dbd2e69f62
Muhammad Dian Nurdiansyah <94899238+DyanHarden@users.noreply.github.com>
2024-05-31 23:28:02 +0800

    fixed artikeladmin.php

--- code/dbadmin/artikeladmin.php
+++ code/dbadmin/artikeladmin.php
@@ -39,7 +39,7 @@ $result = $conn->query($query);
     <link href="dashboard.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
     <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
-    <title>Artikel koontol</title>
+    <title>Artikel Admin</title>
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
